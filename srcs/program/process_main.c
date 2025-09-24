08b7f0cbc23d367fc37c75da73b36de8beebded7
Samuel D'Hooree <samd-hoo@a1-r1-p6.s19.be>
2025-09-24 18:18:54 +0200

    edit/09.24 : added cast ray func

--- srcs/program/process_main.c
+++ srcs/program/process_main.c
@@ -31,7 +31,7 @@ void	draw_wall_line(t_cubed *cubed, int i, t_ray ray, t_tex tex)
 			tex_pos += step;
 			color = *(unsigned int *)(tex.adr + \
 				(ray.tex_y * tex.size_line + ray.tex_x * (tex.bpp / 8)));
-			my_mlx_pixel_put(&cubed->mlx, i, y, color);
+			my_mlx_pixel_put(cubed, i, y, color);
 		}
 	}
 }
@@ -50,6 +50,105 @@ void	first_last_ray(t_cubed *cubed, int i, int rays)
 		cubed->minimap.last_y = cubed->ray.hit_y;
 	}
 }
+
+
+
+double cast_ray(t_cubed *cubed, double angle, int cell)
+{
+    // map dims (tiles)
+    int map_h = 0;
+    while (cubed->data.map[map_h])
+        map_h++;
+    int map_w = 0;
+    for (int r = 0; r < map_h; r++)
+    {
+        int len = (int)strlen(cubed->data.map[r]);
+        if (len > map_w) map_w = len;
+    }
+
+    // player position in tiles (not pixels)
+    double posX = cubed->p.x;
+    double posY = cubed->p.y;
+
+    // ray direction (per tile)
+    double rayDirX = cos(angle);
+    double rayDirY = sin(angle);
+
+    // which box of the map we're in
+    int mapX = (int)posX;
+    int mapY = (int)posY;
+
+    // length of ray from one x or y-side to next x or y-side
+    // guard against zero:
+    double deltaDistX = (fabs(rayDirX) > 1e-9) ? fabs(1.0 / rayDirX) : 1e9;
+    double deltaDistY = (fabs(rayDirY) > 1e-9) ? fabs(1.0 / rayDirY) : 1e9;
+
+    int stepX, stepY;
+    double sideDistX, sideDistY;
+
+    if (rayDirX < 0)
+    {
+        stepX = -1;
+        sideDistX = (posX - mapX) * deltaDistX;
+    }
+    else
+    {
+        stepX = 1;
+        sideDistX = (mapX + 1.0 - posX) * deltaDistX;
+    }
+    if (rayDirY < 0)
+    {
+        stepY = -1;
+        sideDistY = (posY - mapY) * deltaDistY;
+    }
+    else
+    {
+        stepY = 1;
+        sideDistY = (mapY + 1.0 - posY) * deltaDistY;
+    }
+
+    int hit = 0;
+    int side = 0; // 0 = x-side hit, 1 = y-side hit
+
+    // DDA loop
+    while (!hit)
+    {
+        if (sideDistX < sideDistY)
+        {
+            sideDistX += deltaDistX;
+            mapX += stepX;
+            side = 0;
+        }
+        else
+        {
+            sideDistY += deltaDistY;
+            mapY += stepY;
+            side = 1;
+        }
+
+        // bounds check
+        if (mapX < 0 || mapY < 0 || mapX >= map_w || mapY >= map_h)
+            break;
+
+        // map stores chars like '1'/'0'
+        if (cubed->data.map[mapY][mapX] == '1')
+            hit = 1;
+    }
+
+    // compute perpendicular distance (in tiles), robust formula
+    double perpDistTiles;
+    if (side == 0)
+        perpDistTiles = (mapX - posX + (1 - stepX) / 2.0) / ( (fabs(rayDirX) > 1e-9) ? rayDirX : 1e-9 );
+    else
+        perpDistTiles = (mapY - posY + (1 - stepY) / 2.0) / ( (fabs(rayDirY) > 1e-9) ? rayDirY : 1e-9 );
+
+    // convert to absolute and to pixels
+    double perpDistPixels = fabs(perpDistTiles) * cell;
+    return perpDistPixels;
+}
+
+
+
 
 //Main render functie waar alles uit vertrekt
 //Een loop die alle rays doet
@@ -71,7 +170,7 @@ int	render_next_frame(t_cubed *cubed)
 	while (++i < num_rays)
 	{
 		cubed->ray.angle = cubed->p.angle - (cubed->p.fov / 2.0f) + (i * angle_step);
-		cast_ray(cubed, cubed->ray); // FUNCTIE VOOR DE RAY - SAMUEL
+		cast_ray(cubed, cubed->ray.angle, TILE_SIZE); // FUNCTIE VOOR DE RAY - SAMUEL
 		draw_wall_line(cubed, i, cubed->ray, cubed->texture[cubed->ray.side]); // FUNCTIE TEKENEN 3D - PARIS
 		// if (i == 0 || i == num_rays - 1) // MINIMAP
 		// 	first_last_ray(cubed, i, num_rays);// PARIS
